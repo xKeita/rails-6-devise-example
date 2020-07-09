d20507501497bc0eb3f985d908f671d3c7065cee
xKeita <keita.timeleap@gmail.com>
2020-07-10 04:31:02 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'home/index', to: 'home#index'
   get 'friends', to: 'friends#index'
 
   resources :friends
@@ -10,11 +11,11 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'omni_auth'
   }
   devise_scope :user do
-    get 'sign_in', to: 'users/sessions#new'
-    get 'sign_up', to: 'users/registrations#new'
+    get 'login', to: 'users/sessions#new'
+    get 'signup', to: 'users/registrations#new'
     get 'forgot_password', to: 'users/passwords#new'
     get 'reset_password', to: 'users/passwords#edit'
   end
 
-  root to: 'application#home'
+  root to: 'home#index'
 end
