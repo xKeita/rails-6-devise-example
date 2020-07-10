21c2423392cef6ec9b2f4963ea76fcb00f022244
xKeita <keita.timeleap@gmail.com>
2020-07-10 09:38:01 +0900

    bg update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,8 @@ require("@rails/activestorage").start()
 require("channels")
 
 import '../stylesheets/application'
-import 'bootstrap/dist/js/bootstrap'
+// import 'bootstrap/dist/js/bootstrap'
+import '~bootstrap/scss/bootstrap';
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
